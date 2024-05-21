4066454ed1b9f890feaef7024dbd46839e6a5d17
arre-adi <adityachaurasi714@gmail.com>
2024-05-21 23:39:13 +0530

    corusel update

--- homescript.js
+++ homescript.js
@@ -2,89 +2,28 @@ $(document).ready(function () {
     var owl = $(".owl-carousel"); // Define the owl variable
 
     owl.owlCarousel({
-        dots: false,
-        margin: 40,
-        loop: true,
-        // Basic Speeds
-        slideSpeed: 2000,
-        paginationSpeed: 2000,
-
-        // Autoplay
-        autoplay: true,
-        goToFirst: true,
-        goToFirstSpeed: 2000,
-
-        // Navigation
-        navigation: false,
-        navigationText: ["prev", "next"],
-        pagination: false,
-        paginationNumbers: false,
-
-
-        responsive: {
-            0: {
-                items: 3,
-                margin: 10
-            },
-            480: {
-                items: 4
-            },
-            768: {
-                items: 4
-            },
-            992: {
-                items: 4
-            },
-            1200: {
-                items: 6
-            }
-        },
-        autoplay: true, 
-        autoplayTimeout: 2500, 
-        autoplayHoverPause: true 
-    });
-
-  
-    owl.on('mouseenter', function () {
-        owl.trigger('stop.owl.autoplay');
-    });
-
-  
-    owl.on('mouseleave', function () {
-        owl.trigger('play.owl.autoplay', [owl.data('owl.autoplay.timeout')]);
-    });
-});
-
-
-
-
-$(document).ready(function () {
-    var owl = $(".owl-carousel-1"); // Define the owl variable
-
-    owl.owlCarousel({
         dots: true,
-        margin: 0,
-        loop: true,
+        loop: false,
         // Basic Speeds
         slideSpeed: 2000,
         paginationSpeed: 2000,
 
         // Autoplay
-        autoplay: true,
+        autoplay: false,
         goToFirst: true,
         goToFirstSpeed: 2000,
 
         // Navigation
-        navigation: false,
+        navigation: true,
         navigationText: ["prev", "next"],
-        pagination: false,
-        paginationNumbers: false,
+        pagination: true,
+        paginationNumbers: true,
 
+        dotsEach: 1, 
 
         responsive: {
             0: {
-                items: 3,
-                margin: 0
+                items: 3
             },
             480: {
                 items: 3
@@ -96,11 +35,11 @@ $(document).ready(function () {
                 items: 3
             },
             1200: {
-                items: 3
+                items: 3.5  
             }
         },
         autoplay: true, 
-        autoplayTimeout: 2500, 
+        autoplayTimeout: 25000, 
         autoplayHoverPause: true 
     });
 
@@ -114,3 +53,7 @@ $(document).ready(function () {
         owl.trigger('play.owl.autoplay', [owl.data('owl.autoplay.timeout')]);
     });
 });
+
+
+
+
