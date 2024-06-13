1faf0e2efebf41a70bdbc16e3b11c5ec52517707
arre-adi <adityachaurasi714@gmail.com>
2024-06-13 23:34:14 +0530

    major update

--- homescript.js
+++ homescript.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 
     owl.owlCarousel({
         dots: true, 
-        loop: false,
+        loop: true,
         // Basic Speeds
         slideSpeed: 1000,
         paginationSpeed: 1000,
 
         // Autoplay
-        autoplay: false,
-        goToFirst: false,
+        autoplay: true,
+        goToFirst: true,
         goToFirstSpeed: 500,
 
         // Navigation
@@ -46,37 +46,38 @@ $(document).ready(function () {
   var owl = $(".owl-carousel-2"); // Define the owl variable
 
   owl.owlCarousel({
-      dots: true, 
-      loop: false,
-      // Basic Speeds
-      slideSpeed: 500,
-      paginationSpeed: 500,
+      
+    dots: true, 
+        loop: true,
+        // Basic Speeds
+        slideSpeed: 1000,
+        paginationSpeed: 1000,
 
-      // Autoplay
-      autoplay: false,
-      goToFirst: false,
-      goToFirstSpeed: 500,
+        // Autoplay
+        autoplay: true,
+        goToFirst: true,
+        goToFirstSpeed: 500,
 
-      // Navigation
-      navigation: true,
-      navigationText: ["prev", "next"],
-      pagination: true,
-      paginationNumbers: false,
+        // Navigation
+        navigation: true,
+        navigationText: ["prev", "next"],
+        pagination: false,
+        paginationNumbers: false,
 
 
-      dotsEach: 1, 
+        dotsEach: 1, 
 
-      responsive: {
-          0: {
-            items: 2.5  // 2 items on small screens
-          },
-          768: {
-            items: 4  // 3 items on medium screens
-          },
-          1200: {
-            items: 5 // 4 items on large screens (adjust if needed)
+        responsive: {
+            0: {
+              items: 2.5  
+            },
+            768: {
+              items: 4 
+            },
+            1200: {
+              items: 4 
+            }
           }
-        }
         
    
   });
